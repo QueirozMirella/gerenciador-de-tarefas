c15fc4d7f27049bdbac539dfa24558383b7ef1e2
Mirella Queiroz <queiroz.av@gmail.com>
2018-10-05 13:59:14 -0300

    scaffold e validações

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :tarefas
-  root 'tarefas#index'
+	root 'tarefas#index'
+	resources :tarefas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
